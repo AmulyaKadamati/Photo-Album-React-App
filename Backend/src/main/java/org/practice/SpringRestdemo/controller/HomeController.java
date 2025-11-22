6208c47ca788c9c5dcf0c09c0d5e264827fe35a8
Kadamati Amulya <amulya03@Kadamatis-MacBook-Air.local>
2025-11-22 18:29:35 +0530

    changes

--- Backend/src/main/java/org/practice/SpringRestdemo/controller/HomeController.java
+++ Backend/src/main/java/org/practice/SpringRestdemo/controller/HomeController.java
@@ -1,6 +1,5 @@
 package org.practice.SpringRestdemo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
